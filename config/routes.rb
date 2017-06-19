a1720def17d11b9299afb4f950cb7534b3937e6d
Larry Reid <lcreid@jadesystems.ca>
2017-06-19 12:37:56 -0700

    Retroalimentacion de Angela.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "solicitantes#new"
+  root "bienvenida#index"
 
   resources :solicitantes, only: [:new, :create]
+  resources :bienvenida, only: [:index]
 end
