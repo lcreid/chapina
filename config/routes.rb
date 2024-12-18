d3a7c759981d801afa2370455e786e464d207ffb
Larry Reid <lcreid@jadesystems.ca>
2024-12-18 04:54:15 +0000

    Add route for helath check

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   resources :contactos, only: [:create, :new]
   resources :encuestas, only: [:create, :new]
   resources :quienes_somos, only: [:index]
+
+ # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+ get "up" => "rails/health#show", :as => :rails_health_check
 end
